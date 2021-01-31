48e37d1dfa4f8f3503d9a7a7ad31dc7e67374181
Vadim Storozhuk <vadozy@yahoo.com>
2021-01-31 17:26:16 -0500

    End of Section 08

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -3,15 +3,47 @@ import {
   Context,
   APIGatewayProxyStructuredResultV2,
 } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
+
+const s3 = new S3();
+const bucketName = process.env.PHOTO_BUCKET_NAME!;
+
+async function generateUrl(
+  object: S3.Object
+): Promise<{ filename: string; url: string }> {
+  const url = await s3.getSignedUrlPromise('getObject', {
+    Bucket: bucketName,
+    Key: object.Key,
+    Expires: 24 * 60 * 60,
+  });
+  return {
+    filename: object.Key!,
+    url,
+  };
+}
 
 async function getPhotos(
   event: APIGatewayProxyEventV2,
   context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> {
-  return {
-    statusCode: 200,
-    body: 'Hello from lambda, it is alive!',
-  };
+  console.log('I got the bucket name', bucketName);
+  try {
+    const { Contents: results } = await s3
+      .listObjects({ Bucket: bucketName })
+      .promise();
+    const photos = await Promise.all(
+      results!.map(result => generateUrl(result))
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(photos),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message,
+    };
+  }
 }
 
 export { getPhotos };
--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -4,6 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { RemovalPolicy } from '@aws-cdk/core';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import * as path from 'path';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,14 +13,34 @@ export class SimpleAppStack extends cdk.Stack {
 
     const bucket = new Bucket(this, 'MySimpleAppBucket', {
       removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       encryption: BucketEncryption.S3_MANAGED,
+    });
+
+    new BucketDeployment(this, 'MySimpleAppPhotos', {
+      sources: [Source.asset(path.join(__dirname, '..', 'photos'))],
+      destinationBucket: bucket,
     });
 
     const getPhotos = new lambda.NodejsFunction(this, 'MySimpleAppLambda', {
       runtime: Runtime.NODEJS_12_X,
       entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
       handler: 'getPhotos',
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName,
+      },
     });
+
+    const bucketContainerPermissions = new PolicyStatement();
+    bucketContainerPermissions.addResources(bucket.bucketArn);
+    bucketContainerPermissions.addActions('s3:ListBucket');
+
+    const bucketPermissions = new PolicyStatement();
+    bucketPermissions.addResources(`${bucket.bucketArn}/*`);
+    bucketPermissions.addActions('s3:GetObject', 's3:PutObject');
+
+    getPhotos.addToRolePolicy(bucketContainerPermissions);
+    getPhotos.addToRolePolicy(bucketPermissions);
 
     new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
       value: bucket.bucketName,
